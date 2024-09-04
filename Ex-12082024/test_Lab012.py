ba189d620f34c564c2fa8357f5eb0c3841b512a5
lalima <lalimarajput@gmail.com>
2024-09-04 17:07:10 +0530

    Selenium concepts, Selenium wait, Locators

--- Ex-12082024/test_Lab012.py
+++ Ex-12082024/test_Lab012.py
@@ -1,10 +1,10 @@
-
 import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import allure
+
 
 @pytest.mark.positive
 @allure.title("VWO Invalid Login Page - test_mini_project_2")
@@ -27,7 +27,7 @@ def test_mini_project_2():
     # data-qa="hocewoqisi"
     # >
 
-    email_web_element = driver.find_element(By.NAME,"username")
+    email_web_element = driver.find_element(By.NAME, "username")
     email_web_element.send_keys("admin@admin.com")  #to enter input value from keyboard
 
     # <input
@@ -41,15 +41,14 @@ def test_mini_project_2():
     password_web_element = driver.find_element(By.CSS_SELECTOR, "[data-qa='jobodapuxe']")
     password_web_element.send_keys("admin@123")
 
-    # <button type="submit" id="js-login-btn" class="btn btn--positive btn--inverted W(100%) H(48px) Fz(16px)" onclick="login.login(event)" data-qa="sibequkica">
-    # 									<span class="icon loader hidden" data-qa="zuyezasugu"></span>
-    # 									<span data-qa="ezazsuguuy">Sign in</span>
-    # 								</button>
+    # <button type="submit" id="js-login-btn" class="btn btn--positive btn--inverted W(100%) H(48px) Fz(16px)"
+    # onclick="login.login(event)" data-qa="sibequkica"> <span class="icon loader hidden"
+    # data-qa="zuyezasugu"></span> <span data-qa="ezazsuguuy">Sign in</span> </button>
 
     submit_btn_web_element = driver.find_element(By.ID, "js-login-btn")
     submit_btn_web_element.click()
 
-    time.sleep(3) # This is super bad practice - Python Int to stop
+    time.sleep(3)  # This is super bad practice - Python Int to stop
     # Webdirver to stop for the element
 
     error_message_web_element = driver.find_element(By.ID, "js-notification-box-msg")
